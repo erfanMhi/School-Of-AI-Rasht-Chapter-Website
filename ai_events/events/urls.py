b6916bc682aeeb49c79594f749ab7bf77f66edf4
erfan <mhi.erfan1@gmail.com>
2019-01-30 09:19:10 +0330

    Front end fully added.

--- ai_events/events/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('<int:event_id>/', views.event_details, name='event_details'),
-]
--- ai_events/events/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render, get_object_or_404
-
-from .models import Event
-
-
-
-def event_details(request, event_id):
-    event = get_object_or_404(Event, pk=event_id)
-    return render(request, 'events/event-page.html', {'event': event})
